c1f098d3021e72eefc7b9bb38ba838c5d5d3c5c1
krk324 <krk32488@gmail.com>
2014-06-17 19:01:19 -0400

    Add credit_check method.

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -7,4 +7,20 @@ class Tenant
     @age = age
     @credit_score = credit_score
   end
+
+  def credit_rating
+    case
+    when credit_score >= 760
+      'excellent'
+    when credit_score >= 725
+      'great'
+    when credit_score >= 660
+      'good'
+    when credit_score >= 560
+      'mediocre'
+    when credit_score < 560
+      'bad'
+    end
+  end
+
 end
--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -10,4 +10,21 @@ describe Tenant do
       expect(tenant.credit_score).to eq 800
     end
   end
+  describe '#credit_rating' do
+    it '#check credit score' do
+      tenant = Tenant.new('Bob',27,800)
+      tenant2 = Tenant.new('Bob',27,725)
+      tenant3 = Tenant.new('Bob',27,660)
+      tenant4 = Tenant.new('Bob',27,560)
+      tenant5 = Tenant.new('Bob',27,500)
+
+      expect(tenant.credit_rating).to eq 'excellent'
+      expect(tenant2.credit_rating).to eq 'great'
+      expect(tenant3.credit_rating).to eq 'good'
+      expect(tenant4.credit_rating).to eq 'mediocre'
+      expect(tenant5.credit_rating).to eq 'bad'
+
+
+    end
+  end
 end
