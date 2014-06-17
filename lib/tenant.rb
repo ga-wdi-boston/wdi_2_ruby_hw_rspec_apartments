266d41f9f4addadef6360262da704c1d465993e1
csb324 <csb324@gmail.com>
2014-06-17 17:59:28 -0400

    Add tenant factory

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,6 +1,6 @@
 class Tenant
   attr_reader :name, :age, :credit_score
-  def initialize(name, age, credit_score)
+  def initialize(name:, age:, credit_score:)
     @name, @age, @credit_score = name, age, credit_score
   end
 end
--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -4,15 +4,23 @@ describe Tenant do
   # has a name, age, and credit score
   describe '#initialize' do
     it 'has a name, age, and credit score' do
-      person = Tenant.new("Clara", 22, 700)
+      person = Tenant.new(name: "Clara", age: 22, credit_score: 700)
 
       expect(person.name).to eq "Clara"
       expect(person.age).to eq 22
       expect(person.credit_score).to eq 700
 
     end
+
+    it 'is using the factory method' do
+      expect(new_tenant.name).to eq "bob"
+      expect(new_tenant.age).to eq 30
+    end
+
   end
 
   # has a credit rating calculated from chart
-
+  def new_tenant(name: "bob", age: 30, credit_score: 600)
+    tenant = Tenant.new(name: name, age: age, credit_score: credit_score)
+  end
 end
