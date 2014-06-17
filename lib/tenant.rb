58b6d1653c52b22e0aa11015ba35ea5659924566
EricRho <eric.rho.87@gmail.com>
2014-06-17 19:58:23 -0400

    Create initialize to meet test perameters

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,2 +1,14 @@
+
+
+
 class Tenant
+
+  attr_accessor :name, :age, :credit_score
+
+  def initialize(name, age, credit_score)
+    @name = name
+    @age = age
+    @credit_score = credit_score
+  end
+
 end
