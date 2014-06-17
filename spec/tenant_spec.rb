ce21b4d41dfbcf596cd291fcfd7b2f49214606c2
csb324 <csb324@gmail.com>
2014-06-17 17:55:25 -0400

    Add tenant initialize method

--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -2,6 +2,17 @@ require_relative '../lib/tenant'
 
 describe Tenant do
   # has a name, age, and credit score
+  describe '#initialize' do
+    it 'has a name, age, and credit score' do
+      person = Tenant.new("Clara", 22, 700)
+
+      expect(person.name).to eq "Clara"
+      expect(person.age).to eq 22
+      expect(person.credit_score).to eq 700
+
+    end
+  end
+
   # has a credit rating calculated from chart
 
 end
