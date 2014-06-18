b5fa26b818dae5127b78d47713c4ffb47d47a425
Alex Grant <grantovich@users.noreply.github.com>
2014-06-17 23:10:27 -0400

    Apartments have tenants (empty for now)

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -4,5 +4,10 @@ class Apartment
   def initialize(number:, rent:, square_feet:, bedrooms:, bathrooms:)
     @number, @rent = number, rent
     @square_feet, @bedrooms, @bathrooms = square_feet, bedrooms, bathrooms
+    @tenants = []
+  end
+
+  def tenants
+    @tenants.dup # Return a duplicated copy, so the original remains private
   end
 end
--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -1,6 +1,15 @@
 require_relative '../lib/apartment'
+require_relative '../lib/tenant'
 
 describe Apartment do
+  def new_apartment(number: '1B', rent: 2000, square_feet: 600, bedrooms: 1, bathrooms: 1)
+    Apartment.new(number: number, rent: rent, square_feet: square_feet, bedrooms: bedrooms, bathrooms: bathrooms)
+  end
+
+  def new_tenant(name: 'x', age: 30, credit_score: 700)
+    Tenant.new(name: name, age: age, credit_score: credit_score)
+  end
+
   describe '#initialize' do
     it 'requires a number, rent, square footage, bedrooms, and bathrooms' do
       apartment = Apartment.new(
@@ -16,6 +25,20 @@ describe Apartment do
       expect(apartment.square_feet).to eq 820
       expect(apartment.bedrooms).to eq 2
       expect(apartment.bathrooms).to eq 1
+    end
+  end
+
+  describe '#tenants' do
+    it 'is initially empty' do
+      expect(new_apartment.tenants).to be_empty
+    end
+
+    it 'cannot be modified directly' do
+      apartment = new_apartment
+
+      apartment.tenants.push(new_tenant)
+
+      expect(apartment.tenants).to be_empty
     end
   end
 end
