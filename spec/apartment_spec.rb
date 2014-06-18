8eff092c54121fd005dd695abb68f764cf33fc80
testkitschen <katryn.mcintosh@gmail.com>
2014-06-18 08:47:36 -0400

    Create an attribute test for an array of all tenants

--- spec/apartment_spec.rb
+++ spec/apartment_spec.rb
@@ -3,14 +3,15 @@ require_relative "../lib/apartment"
 describe Apartment do
 
   describe 'attributes' do
-    it 'has a number, rent, square footage, number of bedrooms and number of bathrooms' do
-      apartment = Apartment.new(1, 500, 500, 2, 1)
+    it 'has a number, rent, square footage, number of bedrooms and number of bathrooms, and a list of tenants' do
+      apartment = Apartment.new(1, 500, 500, 2, 1, tenants: [])
 
       expect(apartment.number).to eq 1
       expect(apartment.rent).to eq 500
       expect(apartment.sq_footage).to eq 500
       expect(apartment.bedrooms).to eq 2
       expect(apartment.bathrooms).to eq 1
+      expect(apartment.tenants).to eq []
     end
   end
 end
