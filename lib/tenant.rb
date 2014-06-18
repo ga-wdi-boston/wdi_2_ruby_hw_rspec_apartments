3bde30d8e3e8793aa49219052d422130edc6f3fa
Jodi Detch <jodidetch@gmail.com>
2014-06-17 22:39:07 -0400

    Tenant has a credit rating, calculated from the credit score

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,8 +1,21 @@
 class Tenant
-  attr_accessor :name, :age, :credit_score
+  attr_accessor :name, :age, :credit_score, :credit_rating
 
   def initialize(name, age, credit_score)
     @name, @age, @credit_score = name, age, credit_score
+  end
+
+  def credit_rating
+    case credit_score
+      when (0..559) then
+        "bad"
+      when (560..659) then
+        "mediocre"
+      when (660..724) then
+        "good"
+      else
+        "excellent"
+      end
   end
 
 end
