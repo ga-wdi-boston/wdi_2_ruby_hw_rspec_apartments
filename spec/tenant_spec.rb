81ed6d0d1aa633502e36b6b69e4699b3a5d710a5
Max Kohl <maxkohl810@gmail.com>
2014-06-17 23:14:20 -0400

    create a Tenant with a name, age, and credit_score

--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../lib/tenant'
 
 describe Tenant do
+  describe '#initialize' do
+    it 'creates a tenant with a name, age, and credit score' do
+      tenant = Tenant.new('Max Kohl', 25, 725)
 
+      expect(tenant.name).to eq 'Max Kohl'
+      expect(tenant.age).to eq 25
+      expect(tenant.credit_score).to eq 725
+      # expect(tene)
+    end
+  end
 end
