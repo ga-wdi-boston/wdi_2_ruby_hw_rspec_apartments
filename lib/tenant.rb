2e5f623fea3a35cae48527f6033ae70be2e0f50f
Alex Grant <grantovich@users.noreply.github.com>
2014-06-17 23:00:06 -0400

    Tenants have a credit rating

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -4,4 +4,14 @@ class Tenant
   def initialize(name:, age:, credit_score:)
     @name, @age, @credit_score = name, age, credit_score
   end
+
+  def credit_rating
+    case credit_score
+    when 760..800 then :excellent
+    when 725..759 then :great
+    when 660..724 then :good
+    when 560..659 then :mediocre
+    when 300..559 then :bad
+    end
+  end
 end
--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -10,4 +10,35 @@ describe Tenant do
       expect(tenant.credit_score).to eq 740
     end
   end
+
+  describe '#credit_rating' do
+    def scored_tenant(credit_score)
+      Tenant.new(name: 'x', age: 30, credit_score: credit_score)
+    end
+
+    it 'is excellent with a credit score between 760 and 800' do
+      expect(scored_tenant(800).credit_rating).to eq :excellent
+      expect(scored_tenant(760).credit_rating).to eq :excellent
+    end
+
+    it 'is great with a score between 725 and 759' do
+      expect(scored_tenant(759).credit_rating).to eq :great
+      expect(scored_tenant(725).credit_rating).to eq :great
+    end
+
+    it 'is good with a score between 660 and 724' do
+      expect(scored_tenant(660).credit_rating).to eq :good
+      expect(scored_tenant(724).credit_rating).to eq :good
+    end
+
+    it 'is mediocre with a score between 560 and 659' do
+      expect(scored_tenant(560).credit_rating).to eq :mediocre
+      expect(scored_tenant(659).credit_rating).to eq :mediocre
+    end
+
+    it 'is bad with a score between 300 and 559' do
+      expect(scored_tenant(300).credit_rating).to eq :bad
+      expect(scored_tenant(559).credit_rating).to eq :bad
+    end
+  end
 end
