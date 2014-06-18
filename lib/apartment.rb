03512749d5529394872c0abb8b9502c7ab3637e7
Devin Dreszer <devindreszer@gmail.com>
2014-06-17 20:35:13 -0400

    Refactor apartment class to remove duplicate getter methods

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -10,24 +10,20 @@ class Apartment
     @tenants = []
   end
 
-  def tenants
-    tenants = @tenants
-  end
-
   def add_tenant(tenant)
     raise "Bad credit rating" if tenant.credit_rating == 'bad'
     raise "Apartment full" if tenants.length == num_bedrooms
-    self.tenants << tenant
+    tenants << tenant
   end
 
   def remove_tenant(tenant)
     tenant = tenant.name if tenant.class == Tenant
     raise "Tenant not found" unless tenants.map(&:name).include?(tenant)
-    self.tenants.delete_if { |person| person.name == tenant }
+    tenants.delete_if { |person| person.name == tenant }
   end
 
   def remove_all
-    self.tenants.clear
+    tenants.clear
   end
 
   def credit_score
