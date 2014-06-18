3b673df999700931116a6f4f1c366305a600ebfa
EricRho <eric.rho.87@gmail.com>
2014-06-17 20:13:29 -0400

    Create test for credit rating. Write most basic code possible to pass test

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,6 +1,3 @@
-
-
-
 class Tenant
 
   attr_accessor :name, :age, :credit_score
@@ -9,6 +6,14 @@ class Tenant
     @name = name
     @age = age
     @credit_score = credit_score
+  end
+
+  def credit_rating
+    case
+    when credit_score == 700 then 'good'
+    when credit_score == 500 then 'bad'
+
+    end
   end
 
 end
